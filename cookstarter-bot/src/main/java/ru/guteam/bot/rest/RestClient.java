74d7ca9e9f17715f47a91d89d1626e209e67607a
f4nke <cool-vlad@mail.ru>
2020-11-01 18:30:09 +0300

    fix

--- cookstarter-bot/src/main/java/ru/guteam/bot/rest/RestClient.java
+++ cookstarter-bot/src/main/java/ru/guteam/bot/rest/RestClient.java
@@ -1,17 +1,9 @@
 package ru.guteam.bot.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class RestClient {
