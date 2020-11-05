f1431d7f257c77e3bed0555d826026d49c9981b0
ParadiseRaider <53576339+paradiseraider@users.noreply.github.com>
2020-11-05 14:42:54 +0400

    Изменил авторизацию, чтоб был Bearer + token. Исправил в тестах чтоб указание url было в начале через переменные.

---
+++ cookstarter-picture-service/src/main/java/ru/guteam/picture_service/exception/AuthorizationException.java
@@ -0,0 +1,7 @@
+package ru.guteam.picture_service.exception;
+
+public class AuthorizationException extends RuntimeException {
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
