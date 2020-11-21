d229a2592132364eefdd7856400f08c68ec07bb1
Mikhail <vandaniel@mail.ru>
2020-11-21 22:26:28 +0300

    add RequestLoggingAspect, ErrorController, tests to Order board. refactor main.js and change board.html and fix Dish price

---
+++ cookstarter-order-board/src/test/java/ru/guteam/cookstarterorderboard/controller/util/Url.java
@@ -0,0 +1,9 @@
+package ru.guteam.cookstarterorderboard.controller.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Url {
+    public static final String GET_BOARD = "/board/";
+    public static final String ADD_ORDER = "/add";
+}
